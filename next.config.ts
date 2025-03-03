3b410963368c2cdeb8b666a1cd6e337d6ba35314
Abrak-Adabra <abrak.adabra@internet.ru>
2025-03-03 22:10:27 +0300

    clear app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    basePath: '/kmzi_lr1',
+    output: 'export',
+    devIndicators: false,
+}
 
-export default nextConfig;
+export default nextConfig
