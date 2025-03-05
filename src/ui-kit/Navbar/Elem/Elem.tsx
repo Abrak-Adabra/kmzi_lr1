fce4774caa5cd51bbe2ca99809efa84436d28ac5
Abrak-Adabra <abrak.adabra@internet.ru>
2025-03-05 09:58:41 +0300

    add simple header

--- src/ui-kit/Navbar/Elem/Elem.tsx
+++ src/ui-kit/Navbar/Elem/Elem.tsx
@@ -1,15 +1,16 @@
 // import { useState } from 'react'
 import style from './elem.module.scss'
 
-type ElemProps = { children?: React.ReactNode; title: string }
+type ElemProps = { children?: React.ReactNode; title: string; onClick?: () => void }
 
-const Item: React.FC<ElemProps> = ({ children, title }) => {
+const Item: React.FC<ElemProps> = ({ children, title, onClick }) => {
     // const [showChild, setShowChild] = useState<boolean>(false)
     return (
         <div
             className={style['container']}
             // onMouseEnter={() => setShowChild(true)}
             // onMouseLeave={() => setShowChild(false)}
+            onClick={onClick ? onClick : () => {}}
         >
             <div className={style['title']}>{title}</div>
 
